c0cc4e8f7700e40cd12b72aec488af031f8e825f
Matias <56095797+GallegosMatias@users.noreply.github.com>
2020-12-03 19:13:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 const btnEmpezar = document.getElementById('btnEmpezar');
-const ULTIMO_NIVEL = 10;
+const ULTIMO_NIVEL = 3;
 
 class Juego {
   constructor(){
